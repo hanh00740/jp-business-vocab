badaefcc6e6de0b7e5cd6a8498a657ee34b6340a
hanh00740 <hanh00740@users.noreply.github.com>
2026-06-26 16:01:06 +0900

    Batch 6: 13 more koji (classical idioms) + 13 keigo errors (new category keigong); sw v10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache app shell so it works offline (subway, no signal). Bump CACHE when you redeploy.
-const CACHE = "jpbiz-v9";
+const CACHE = "jpbiz-v10";
 const ASSETS = [
   "./",
   "./index.html",
