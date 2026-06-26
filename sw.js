d1ceb5c1d5cefb47a7b99c7c7b8551a99c5d0c54
hanh00740 <hanh00740@users.noreply.github.com>
2026-06-26 10:49:44 +0900

    Add batch 2: 14 keigo verbs + 14 IT terms + 12 grammar patterns (new category keigov); sw v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache app shell so it works offline (subway, no signal). Bump CACHE when you redeploy.
-const CACHE = "jpbiz-v5";
+const CACHE = "jpbiz-v6";
 const ASSETS = [
   "./",
   "./index.html",
