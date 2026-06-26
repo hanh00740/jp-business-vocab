e697e91efc6aea7b25c2f05d0c67974276da6c3b
hanh00740 <hanh00740@users.noreply.github.com>
2026-06-26 15:39:49 +0900

    Batch 5: 15 koji (classical idioms) + 12 goyou (misused words) + 10 advanced expressions; sw v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache app shell so it works offline (subway, no signal). Bump CACHE when you redeploy.
-const CACHE = "jpbiz-v8";
+const CACHE = "jpbiz-v9";
 const ASSETS = [
   "./",
   "./index.html",
